4a8f4c10d0bc2eeebce67811f65b1d10c028f325
Lonlydragon <43724479+Lonlydragon@users.noreply.github.com>
2020-12-27 20:02:07 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,47 @@
+
+import config
+import logging
+import price
+
+from aiogram import Bot, Dispatcher, executor, types
+
+# задаем уровень логов
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=config.TELEGRAM_API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=["new_chat_members"])
+async def send_welcome(message: types.message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    user_name = message.from_user.first_name
+    await message.chat.promote(user_id=message.from_user.id, can_delete_messages=False, can_pin_messages=True,
+                                   can_invite_users=True, can_change_info=True)
+    await message.answer("{} выданы админские права, Lalka".format(user_name))
+    await message.chat.set_administrator_custom_title(user_id=message.from_user.id, custom_title="Lalka")
+
+@dp.message_handler(content_types=["left_chat_member"])
+async def send_welcome(message: types.message):
+    user_name = message.from_user.first_name
+    await message.answer("{} прощай, Lalka".format(user_name))
+
+@dp.message_handler(commands=['usdrub'])
+async def curse(message: types.message):
+    usd_rub = price.get_usd_course()
+    await message.answer("Курс доллара: {}".format(usd_rub))
+
+@dp.message_handler(commands=['tesla'])
+async def curse(message: types.message):
+    tsla = price.get_tsla_price()
+    await message.answer("Курс акциий Tesla: {}".format(tsla))
+
+@dp.message_handler(commands=['apple'])
+async def curse(message: types.message):
+    apple = price.get_apple_price()
+    await message.answer("Курс акций Apple: {}".format(apple))
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ price.py
@@ -0,0 +1,29 @@
+from pycbrf.toolbox import ExchangeRates
+from decimal import Decimal
+from pytz import timezone
+from datetime import datetime
+import yfinance as yf
+
+
+# Текущая дата
+def get_now() -> str:
+    moscow_tz = timezone("Europe/Moscow")
+    return moscow_tz.localize(datetime.now()).strftime("%Y-%m-%d")
+
+# Текущий курс доллара
+def get_usd_course() -> Decimal:
+    rates = ExchangeRates(get_now())
+    return rates['USD'].value
+
+def get_tsla_price() -> str:
+    Tesla = yf.Ticker("TSLA")
+    return Tesla.info['ask']
+
+def get_apple_price() -> str:
+    Apple = yf.Ticker("AAPL")
+    return Apple.info['ask']
+
+
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print(get_usd_course())
